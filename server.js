49889ef1bb0116167f0706832f1501c7385e8f92
jchoi10 <noreply>
2022-11-14 22:49:31 -0500

    updated schema and npm start bug

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ require('dotenv').config();
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: process.env.MYSQL_PASSWORD,
+    password: process.env.DB_PASSWORD,
     database: 'employee_db'
 });
 
 connection.connect(err => {
     if (err) throw err;
-    console.log('connected as ' + connection.threadId);
+    console.log('connected as id ' + connection.threadId);
     console.log('EMPLOYEE MANAGER');
     promptUser();
 });
